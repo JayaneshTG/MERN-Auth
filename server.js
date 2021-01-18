3d19303e6645dca7b54b208f9015e95e1a35c231
jayaneshtg@gmail.com <jayaneshtg@gmail.com>
2021-01-18 22:10:51 +0530

    Final commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 var app = express()
 
 const bodyparser = require('body-parser')
+const port = process.env.PORT || 5000;
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
 
@@ -14,6 +15,13 @@ app.get('/', (req,res)=> {
 	res.send('This is the backend')
 });
 
-app.listen(5000,() => {
+app.listen(port,() => {
 	console.log('Server started on port 5000');
 })
+
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  });
+}
